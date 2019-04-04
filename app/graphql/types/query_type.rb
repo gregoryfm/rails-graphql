dd7e7f66b461995d896a9c4ac89e95441683c2c6
Gregory <gregoryfontenele@gmail.com>
2019-04-05 10:12:46 +1100

    Create UserType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :users, [UserType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def users
+      User.all
     end
   end
 end
